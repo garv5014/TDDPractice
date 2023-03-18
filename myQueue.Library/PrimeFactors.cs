b6688be111531e64a56a1f43eb3e56bc001ea253
garv5014 <garv5014@gmail.com>
2023-03-17 21:34:21 -0600

    added tests up to prime factor of 4

---
+++ myQueue.Library/PrimeFactors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDProjects.Library;
+
+public class PrimeFactors
+{
+    public List<int> FindFactors(int n)
+    {
+        var factors = new List<int>();
+        if (n % 2 == 0)
+        {
+            factors.Add(n);
+        }
+        return factors;
+    }
+}
---
+++ myQueue.Test/PrimeFactorTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDProjects.Test;
+
+public class PrimeFactorTests
+{
+    private PrimeFactors _primeFactors;
+    [SetUp]
+    public void Setup()
+    {
+        _primeFactors = new PrimeFactors();
+    }
+    [Test]
+    public void PrimeFactorOfOne()
+    {
+        _primeFactors.FindFactors(1).Should().BeEmpty();
+    }
+    [Test]
+    public void PrimeFactorOfTwo()
+    {
+        _primeFactors.FindFactors(2).Should().Contain(2);
+    }
+
+    [Test]
+    public void PrimeFactorOfThree()
+    {
+        _primeFactors.FindFactors(3).Should().Contain(3);
+    }
+
+    [Test]
+    public void PrimeFactorOfFour()
+    {
+        _primeFactors.FindFactors(4).Should().Contain(new int[] { 2,2 } );
+    }
+}
